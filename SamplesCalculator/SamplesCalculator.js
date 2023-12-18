be974db12717a9b11266287da81a0a037316cc46
Lucifer <selonglucifer@163.com>
2023-12-18 20:55:21 +0800

    Create SamplesCalculator.js

---
+++ SamplesCalculator/SamplesCalculator.js
@@ -0,0 +1,51 @@
+let inputs = document.getElementsByTagName('input')
+// Array.from(inputs).forEach((input) => {
+//     input.addEventListener('keydown', function (event) {
+//         if (event.keyCode == 13) {
+//             focusNextInput(this);
+//         }
+//     });
+// })
+
+function calculate() {
+    let startTime = document.getElementById('starttime').value.split('.')
+    let endTime = document.getElementById('endtime').value.split('.')
+    let timeLeak = document.getElementById('timeleak').value
+    let samples = document.getElementById('samples')
+
+    startTime = (Number(startTime[0]) * 60 + Number(startTime[1])) * 60 + Number(startTime[2])
+    endTime = (Number(endTime[0]) * 60 + Number(endTime[1])) * 60 + Number(endTime[2])
+
+    samples.value = (endTime - startTime + 1) * 10 - Number(timeLeak)
+    // alert(startTime)
+}
+
+function clearInput() {
+    document.getElementById('starttime').value = ''
+    document.getElementById('endtime').value = ''
+    document.getElementById('timeleak').value = ''
+    document.getElementById('samples').value = ''
+}
+
+// function focusNextInput(thisInput) {
+
+//     // for (let i = 0; i < inputs.length - 1; i++) {
+//     //     if (thisInput == inputs[inputs.length - 2]) {
+//     //         inputs[0].focus();
+//     //         break;
+//     //     } else if (thisInput == inputs[i]) {
+//     //         inputs[i + 1].focus();
+//     //         break;
+//     //     } else {
+//     //         document.getElementById('calculate').click();
+//     //     }
+//     // }
+//     for (let i = 0; i < inputs.length - 1; i++) {
+//         if (thisInput == inputs[inputs.length - 2]) {
+//             document.getElementById('calculate').focus();
+//             break;
+//         } else {
+//             inputs[i + 1].focus();
+//         }
+//     }
+// }
